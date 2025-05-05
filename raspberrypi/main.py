c8ec058e639ac737fc9d1c5db05f106247187543
Anthony Aldrin Beltran <32714974+aabeltran0321@users.noreply.github.com>
2025-05-05 20:44:02 +0800

    Update main.py

--- raspberrypi/main.py
+++ raspberrypi/main.py
@@ -1,17 +1,40 @@
 import requests
+from Parser_WTA import Parser
 
-# URL endpoint
-url = "https://bigboysautomation.pythonanywhere.com/tupmicrogreens/hilo"
+tupmParser = Parser("TUPM,", "\r", 1, 200)
+tupmParser1 = Parser("TUPM,", "\n", 1, 200)
+def get_hilo():
+    # URL endpoint
+    url = "https://bigboysautomation.pythonanywhere.com/tupmicrogreens/hilo"
+    # Send POST request
+    response = requests.post(url, verify=False)
 
-# Example payload — replace with actual expected data
-data = {
-    "key1": "value1",
-    "key2": "value2"
-}
+    return response.json()
 
-# Send POST request
-response = requests.post(url, verify=False)
 
-# Print response
-print("Status Code:", response.status_code)
-print("Response JSON:", response.text)
+with open("test.csv", "r", encoding="utf8") as f:
+    Serial1 = f.read()
+    f.close
+
+
+for c in Serial1:
+    if tupmParser.available(c) or tupmParser1.available(c):
+        # hilo1 = get_hilo()
+
+
+        # for data in hilo1:
+        #     low_value = hilo1[data]["low"]
+        #     hi_value = hilo1[data]["high"]
+
+        #     print(hi_value, low_value)
+        if len(tupmParser.data):
+            data = tupmParser.data.replace("\r","")
+            data = data.replace("\n", "")
+            data = data.split(",")
+            
+        elif len(tupmParser1.data):
+            data = tupmParser.data.replace("\r", "")
+            data = data.replace("\n", "")
+            data = data.split(",")
+
+        print(data)
