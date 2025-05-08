9095202a5a2ce829d5bf816bb09cd44a4f5d3020
Beltran <1PH114@INNODATA.COM>
2025-05-08 12:55:40 +0800

    update codes

--- raspberrypi/main.py
+++ raspberrypi/main.py
@@ -1,8 +1,21 @@
 import requests
 from Parser_WTA import Parser
-
+import time
 tupmParser = Parser("TUPM,", "\r", 1, 200)
 tupmParser1 = Parser("TUPM,", "\n", 1, 200)
+
+class Scheduler:
+    def __init__(self,millis):
+        self.start_time = time.time()
+        self.sec = 0.0
+        self.sec=millis/1000
+    def Event(self):
+        if (time.time() - self.start_time) > self.sec:
+            self.start_time = time.time()
+            return True
+        else:
+            return False
+
 def get_hilo():
     # URL endpoint
     url = "https://bigboysautomation.pythonanywhere.com/tupmicrogreens/hilo"
@@ -40,11 +53,7 @@ dev_tty = "COM2"
 
 dict1 = get_hilo()
 
-commands = {
-    'ORP': 'OZON',
-    'EC': 'PPU3',
-    'pH Level': 'PPU1',
-}
+
 
 for c in Serial2:
     data = ParsingProcess(c)
@@ -58,8 +67,39 @@ for c in Serial2:
 
         
         for k in dict1:
-            low = dict1[k]['low']
-            high = dict1[k]['high']
+            low = float(dict1[k]['low'])
+            high = float(dict1[k]['high'])
             val = dict2[k]
-            print(val, k,low < val < high)
+            command = ""
+            if k == "Humidity":
+                if val > high:
+                    command = "DEHU1"
+                else:
+                    command = "DEHU0"
+            elif k == "ORP":
+                if val < low:
+                    command = "OZON1"
+                else:
+                    command = "OZON0"
+
+            elif k == "Temperature":
+                if val > high:
+                    command = ["FAN11","FAN21", "FAN31"]
+                else:
+                    command = ["FAN10","FAN20", "FAN30"]
+
+            elif k == "pH Level":
+                if val > high:
+                    command = ["PPU21", "PPU10"]
+
+                if val < low:
+                    command = ["PPU11", "PPU20"]
+                else:
+                    command = ["PPU10", "PPU20"]
+
+            if isinstance(command, str):
+                print(command)
+            else:
+                for cmd in command:
+                    print(cmd)
         break
